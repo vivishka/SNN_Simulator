36b4416583106891204f27daca88fa3e8fe7e5e6
Vivien <vmonneret30@gmail.com>
2019-05-17 11:10:54 +0900

    Added the sparse class

---
+++ classes/sparse.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+
+class Sparse(object):
+    """
+    Sparse way of storing the weights
+
+    :param mat: np.ndarray. Dense matrix containg the weights an a lot of zeros
+
+    """
+    def __init__(self, mat):
+        self.shape = mat.shape
+
+        self.mat = []
+
+        nb_non_zero = 0
+        for i in range(self.shape[0]):
+            row = []
+            for j in range(self.shape[1]):
+                if mat[i, j] != 0:
+                    row.append((i, j, mat[i, j]))
+                    nb_non_zero += 1
+            self.mat.append(row)
+
+        self.size = nb_non_zero
+
+    def todense(self):
+        mat = np.zeros(self.shape)
+        for row in self.mat:
+            for data in row:
+                mat[data[0], data[1]] = data[2]
+        return mat
+
+    def __getitem__(self, item):
+        if isinstance(item, int):
+            return self.mat[item]
+        elif isinstance(item, tuple) and len(item) == 2:
+            row = self.mat[item[0]]
+            # todo: optimisation dichotomy
+            for data in row:
+                if data[1] == item[1]:
+                    return data[2]
+        return 0
+
+    def __setitem__(self, key, value):
+        if isinstance(key, int):
+            self.mat = value
+        elif isinstance(key, tuple) and len(key) == 2:
+            row = self.mat[key[0]]
+            # todo: optimisation dichotomy
+            for j, data in enumerate(row):
+                if data[1] == key[1]:
+                    row[j] = (key[0], key[1], value)
+
+    def get_kernel(self, index, length, kernel_size):
+        """
+        rebuild the kernel weights
+        :param index: int or (int, int). Center neuron index
+        :param length: Length of the layer (dimension 0)
+        :param kernel_size: (int, int).
+        :return: np.ndarray containing the extracted weights around the index
+        """
+        if isinstance(index, int):
+            index_1d = index
+            index_2d = Sparse.get_index_2d(index, length)
+        elif isinstance(index, tuple) and len(index) == 2:
+            index_1d = Sparse.get_index_1d(index, length)
+            index_2d = index
+        else:
+            return
+        kernel = np.zeros(kernel_size)
+        for row in range(kernel_size[1]):
+            for col in range(kernel_size[1]):
+                x = index_2d[0] + row - kernel_size[0] // 2
+                y = index_2d[1] + col - kernel_size[1] // 2
+                try:
+                    kernel[row, col] = self[(index_1d, Sparse.get_index_1d((x, y), length))]
+                except IndexError:
+                    pass
+        return kernel
+
+    @staticmethod
+    def get_index_2d(index_1d, length):
+        """ returns the (row, column) for a given index and length
+        ex index = 42, length = 10 => row = 4, col = 2
+        """
+        return index_1d // length, index_1d % length
+
+    @staticmethod
+    def get_index_1d(index_2d, length):
+        """ returns the index for a given (row, column) and length
+        ex index = (4, 2), length = 10 => index_1D = 42
+        """
+        return index_2d[0] * length + index_2d[1]
