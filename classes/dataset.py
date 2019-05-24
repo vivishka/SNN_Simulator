62a8313896cfede1ee32e623f6a27ee5dc760214
Corentin <corentin.lelez@gmail.com>
2019-05-24 17:55:32 +0900

    added first experiment dataset generator

--- classes/dataset.py
+++ classes/dataset.py
@@ -7,7 +7,7 @@ sys.dont_write_bytecode = True
 
 class Dataset(object):
 
-    def __init__(self, path, index=0):
+    def __init__(self, path='', index=0):
         self.index = index
         self.data = []
         self.labels = []
@@ -33,10 +33,40 @@ class Dataset(object):
         return self.data[index]
 
 
+class VectorDataset(Dataset):
+
+    def __init__(self, path='', index=0, size=50):
+        super(VectorDataset, self).__init__(path, index)
+        self.size = size
+
+    def load(self):
+        for input_data in range(self.size):
+            exp = self.generator()
+            self.labels.append(exp[0])
+            self.data.append(exp[1])
+
+    def generator(self):
+        pass
+
+
+class Exp1Dataset(VectorDataset):
+
+    def __init__(self, path='', index=0, size=50, width=0.25, gap=0.5):
+        super(Exp1Dataset, self).__init__(path, index, size)
+        self.width = width
+        self.gap = gap
+        self.load()
+
+    def generator(self):
+        cat = np.random.randint(0, 1)
+        data = self.width * np.random.rand() + (self.gap + self.width)*cat
+        return cat, data
+
+
 class ImageDataset(Dataset):
 
-    def __init__(self, file, index=0, size=(28, 28)):
-        super(ImageDataset, self).__init__(file, index)
+    def __init__(self, path='', index=0, size=(28, 28)):
+        super(ImageDataset, self).__init__(path, index)
         self.size = size
         self.load()
 
