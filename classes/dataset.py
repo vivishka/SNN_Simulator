e5831ab24bdb0d9e020fab720d74f81d471dc2f4
Vivien <vmonneret30@gmail.com>
2019-06-05 13:46:15 +0900

    added create_pattern_dataset

--- classes/dataset.py
+++ classes/dataset.py
@@ -81,3 +81,57 @@ class ImageDataset(Dataset):
             self.labels.append(np.array(string[0]).astype(np.uint8))
         self.file.close()
         Helper.log('Dataset', log.INFO, 'reading file done')
+
+
+def pattern_image_generator(size, pattern_list, nb_features):
+    mat = np.zeros(size, dtype=float)
+    nb_div = int(np.ceil(np.sqrt(nb_features)))
+    x_div = size[0] // nb_div
+    y_div = size[1] // nb_div
+    i = 0
+    order = [index % len(pattern_list) for index in range(nb_features)]
+    np.random.shuffle(order)
+    for row in range(nb_div):
+        for col in range(nb_div):
+            if i >= nb_features:
+                break
+            feature = pattern_list[order[i]]
+            s = feature.shape
+            x0 = np.random.randint(row * x_div, (row + 1) * x_div - s[0])
+            y0 = np.random.randint(col * y_div, (col + 1) * y_div - s[0])
+            mat[x0:x0+s[0], y0:y0+s[1]] += feature
+            i += 1
+
+    for i in range(size[0]):
+        for j in range(size[1]):
+            if mat[i, j] == 1:
+                mat[i, j] = np.random.randn() * 0.1 + 0.75
+            else:
+                mat[i, j] = np.random.randn() * 0.1 + 0.25
+
+    mat = np.clip(mat, 0, 1) * 255
+    return mat.astype('uint8')
+
+
+def create_pattern_dataset(path, nb_images, size, nb_features):
+    k1 = np.array([[1], [1], [1]], dtype=int)
+    k2 = np.array([[1, 1, 1]], dtype=int)
+    k3 = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]], dtype=int)
+    k4 = np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]], dtype=int)
+
+    file = open(path, 'w')
+    file.write('\n')
+
+    for i in range(nb_images):
+        data = pattern_image_generator(size, [k3, k4], nb_features)
+        if nb_images == 1:
+            plt.figure()
+            plt.imshow(data, cmap='gray')
+        data = data.flatten()
+        line = '0,' + ",".join(str(x) for x in data)
+        file.write(line)
+        file.write('\n')
+
+    if nb_images == 1:
+        plt.show()
+    file.close()
