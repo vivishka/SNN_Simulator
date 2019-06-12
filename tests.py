c3d50c785b60387ffda0e4d3a85e7e26dd63a754
Corentin <corentin.lelez@gmail.com>
2019-06-12 15:03:34 +0900

    dog encoder rework, file dataset length added

--- tests.py
+++ tests.py
@@ -27,13 +27,14 @@ mpl_logger = log.getLogger('matplotlib')
 mpl_logger.setLevel(log.WARNING)
 
 
-filename = 'datasets/fashionmnist/fashion-mnist_test.csv'
+filename = 'datasets/fashionmnist/fashion-mnist_train.csv'
 img_size = (28, 28)
-first_image = 8
-image_dataset = FileDataset(filename, first_image, size=img_size)
+first_image = 1
+image_dataset = FileDataset(filename, first_image, size=img_size, length=10)
 
 model = Network()
-e1 = EncoderDoG(depth=1, size=img_size, in_min=0, in_max=255, delay_max=1)
+e1 = EncoderDoG(sigma=[(3/9, 6/9), (7/9, 14/9), (13/6, 26/9)],
+                kernel_sizes=[3, 7, 13], size=img_size, in_min=0, in_max=255, delay_max=1, threshold=0.2)
 n1 = Node(e1, image_dataset, 1, 0)
 b1 = Bloc(4, img_size, IF())
 
@@ -41,12 +42,17 @@ d1 = Decoder(img_size)
 
 c1 = Connection(e1, b1, kernel=(1, 1))
 
-Connection(b1, d1, kernel=1)
+c2 = Connection(b1, d1, kernel=1)
 
 
 sim = Simulator(model, 0.02, input_period=1, batch_size=1)
-sim.run(2, monitor_connection=c1, convergence_threshold=0.0005)
+sim.run(10)
 
-image_dataset.plot(1)
-e1.plot(0)
+image_dataset.plot(image_dataset.index)
+for ens in range(6):
+    e1.plot(image_dataset.index, ens)
+
+
+Helper.print_timings()
+
 plt.show()
